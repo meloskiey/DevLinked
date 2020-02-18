0c86f8273b41ef36a20e470a71c674143ac78c28
mel_mak95 <melissamaksuying@gmail.com>
2020-02-18 12:52:40 +0800

    updated auth backend

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User')
@@ -24,7 +26,7 @@ async (req,res) => {
     let user = await User.findOne({ email });
 
     if(user){
-        res.status(400).json({errors: [{msg: 'User already exists'}]});
+        return res.status(400).json({errors: [{msg: 'User already exists'}]});
     }
 
     const avatar = gravatar.url(email,{
@@ -48,8 +50,18 @@ async (req,res) => {
     await user.save();
 
     //Return jsonwebtoken
-    res.send('User registered');
+    const payload = {
+        user: {
+            id: user.id
+        }
+    };
 
+    jwt.sign(payload, config.get('jwtSecret'),
+    { expiresIn: 36000 },
+    (err, token) => {
+        if(err) throw err;
+        res.json({token});
+    });
     } catch(err) {
         console.error(err.message);
         res.status(500).send('Server error');
