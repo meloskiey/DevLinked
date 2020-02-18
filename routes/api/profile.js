cd65971a7019a772549b5e00fd4c3b772116d5cf
mel_mak95 <melissamaksuying@gmail.com>
2020-02-18 19:40:06 +0800

    added delete

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -21,4 +22,129 @@ router.get('/me', auth, async (req,res) => {
     }
 });
 
+router.post('/',[auth,[
+    check('status','Status is required').not().isEmpty(),
+    check('skills', 'Skills is required').not().isEmpty()
+    ]
+],
+async (req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubusername,
+        skills,
+        youtube,
+        facebook,
+        twitter, 
+        instagram,
+        linkedin
+    } = req.body;
+
+    //build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if(company) profileFields.company = company;
+    if(website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields. githubusername = githubusername;
+    if(skills){
+        profileFields.skills = skills.split(',').map(skill => skill.trim());
+    }
+
+    //Build social object
+    profileFields.social = {}
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+    try {
+        let profile = await Profile.findOne({ user:req.user.id })
+
+        if(profile){
+            //Update
+            profile = await Profile.findOneAndUpdate(
+                { user: req.user.id }, 
+                { $set:profileFields },
+                { new: true }
+            );
+
+            return res.json(profile);
+
+        }
+
+        //Create
+        profile = new Profile(profileFields);
+        await profile.save();
+        res.json(profile);
+        } catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+        }
+    }
+);
+
+//GET api/profile
+//Get all profiles
+//Public
+router.get('/',async (req,res)=> {
+    try {
+        const profiles = await Profile.find().populate('user', ['name','avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+        
+    }
+});
+
+//GET api/profile/user/:user_id
+//Get profile by User ID
+//Public
+router.get('/user/:user_id',async (req,res)=> {
+    try {
+        const profile = await Profile.findOne({user: req.params.user_id}).populate('user', 
+        ['name','avatar']);
+
+        if(!profile)
+        return res.status(400).json({msg: 'Profile not found'});
+
+        res.json(profile);
+      } catch (err) {
+        console.error(err.message);
+        if(err.kind == 'ObjectId'){
+            return res.status(400).json({msg: 'Profile not found'});
+        }
+        res.status(500).send('Server Error')  
+    }
+});
+
+//DELETE api/profile/user/:user_id
+//Delete profile by User ID
+//Private
+router.delete('/', auth, async (req,res)=> {
+    try {
+        //Remove users posts
+        //Remove profile
+        await Profile.findOneAndRemove({user: req.user.id});
+        //Remove User
+        await User.findOneAndRemove({_id: req.user.id});
+
+        res.json({msg: 'User deleted'});
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')  
+    }
+});
 module.exports = router;
